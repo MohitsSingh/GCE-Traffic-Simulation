e2dfac1b4e86eaa5ce17f413cf7d119ecabe2aa3
Eric <erictaw2018@u.northwestern.edu>
2016-05-12 01:40:47 -0500

    normalize traffic

--- traffic_sim.m
+++ traffic_sim.m
@@ -2,11 +2,12 @@ adj_mat = xlsread('gce traffic network weight punish.xlsx', 'B2:AO41');
 [~, labels, ~] = xlsread('gce traffic network weight punish.xlsx', 'A2:A41');
 adj_mat(isnan(adj_mat)) = 0;
 
+global traffic;
+traffic = zeros(numedges(G), 1);
+
 G = graph(adj_mat, labels);
-p = plot(G, 'EdgeLabel', G.Edges.Weight, 'LineWidth', 3*G.Edges.Weight);
-% [path1, d] = shortestpath(G, 29, 1);
-% highlight(p, path1, 'EdgeColor', 'r');
-% dist = distances(G);
+% p = plot(G, 'EdgeLabel', G.Edges.Weight, 'LineWidth', 3*G.Edges.Weight);
+
 
 % simulate traffic flow
 
@@ -16,5 +17,10 @@ G = addPeople(G, 18, 19, 10);
 % move 8 people from room6 to dlab
 G = addPeople(G, 39, 11, 8);
 
+G = addPeople(G, 1, 19, 5);
+
+traffic( traffic == 0 ) = 1;
+
+G.Edges.Weight = G.Edges.Weight .* traffic;
 
 p = plot(G, 'EdgeLabel', G.Edges.Weight, 'LineWidth', G.Edges.Weight./8);
