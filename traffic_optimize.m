fd77e8afa4ad780a31b83109021bbe9d6783681f
Eric <erictaw2018@u.northwestern.edu>
2016-05-16 14:15:36 -0500

    scores working, code cleanup

--- traffic_optimize.m
+++ traffic_optimize.m
@@ -4,23 +4,35 @@ adj_mat(isnan(adj_mat)) = 0;
 
 rooms = [17, 18, 35, 36, 38, 39];
 
-global traffic;
-traffic = zeros(numedges(G), factorial(length(rooms))-1);
-
 G = graph(adj_mat, labels);
-% p = plot(G, 'EdgeLabel', G.Edges.Weight, 'LineWidth', 3*G.Edges.Weight);
 
+global traffic;
+traffic = zeros(numedges(G), 1);
 
-% optimize traffic flow
+combinations = nchoosek(rooms, 3);
 
+scores = zeros(size(combinations, 1), 1);
 
-for i = 1:length(rooms)-1
-    from = rooms;
-    to = 
+for i = 1:size(combinations, 1)
+    paths = combinations(i, :);
+    
+    new_graph = G;
+    
+    new_graph = addPeople(new_graph, paths(1), paths(2), 10);
+    new_graph = addPeople(new_graph, paths(2), paths(3), 10);
+    new_graph = addPeople(new_graph, paths(3), paths(1), 10);
+    
+    new_graph.Edges.Weight = new_graph.Edges.Weight .* traffic;
+    
+%     W = new_graph.Edges.Weight( new_graph.Edges.Weight > 3 );
+
+    score = sum(new_graph.Edges.Weight);
+    scores(i) = score;
+    
+    % reset traffic
+    traffic = zeros(numedges(G), 1);
 end
 
 traffic( traffic == 0 ) = 1;
-
-G.Edges.Weight = G.Edges.Weight .* traffic;
 
 p = plot(G, 'EdgeLabel', G.Edges.Weight, 'LineWidth', G.Edges.Weight./8);
--- traffic_sim.m
+++ traffic_sim.m
@@ -2,10 +2,11 @@ adj_mat = xlsread('gce traffic network weight punish.xlsx', 'B2:AO41');
 [~, labels, ~] = xlsread('gce traffic network weight punish.xlsx', 'A2:A41');
 adj_mat(isnan(adj_mat)) = 0;
 
+G = graph(adj_mat, labels);
+
 global traffic;
 traffic = zeros(numedges(G), 1);
 
-G = graph(adj_mat, labels);
 % p = plot(G, 'EdgeLabel', G.Edges.Weight, 'LineWidth', 3*G.Edges.Weight);
 
 
